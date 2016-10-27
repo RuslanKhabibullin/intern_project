4570cc53a71b35a639be5c349806ad31d8e10dad
Ozone22 <willfarm@bk.ru>
2016-10-27 19:24:04 +0300

    Devise tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-    root 'devise/sessions#new'
+    root 'devise/registrations#edit'
   end
 end
--- spec/requests/devise_pages_spec.rb
+++ spec/requests/devise_pages_spec.rb
@@ -23,6 +23,9 @@ describe 'Devise pages' do
     context 'when registration is success' do
       before do
         user_params = FactoryGirl.attributes_for(:user)
+
+        # Refactor needed(Formulaic throws errors)!
+
         fill_in 'Username', with: user_params[:username]
         fill_in 'Email', with: user_params[:email]
         fill_in 'Password', with: user_params[:password]
@@ -31,7 +34,7 @@ describe 'Devise pages' do
       end
 
       it { should have_selector('div.alert.alert-notice') }
-      it { should have_content('Success') }
+      it { should have_content('Welcome') }
       it { should have_content('Articles') }
 
     end
@@ -40,6 +43,8 @@ describe 'Devise pages' do
   describe 'SignIn page' do
     before { visit new_user_session_path }
 
+    # Remove to shared_example. But how to name? it_behaves_like 'signup_page_fields' ?
+
     it { should have_field('Email') }
     it { should have_field('Password') }
     it { should have_button('SignIn') }
@@ -47,7 +52,7 @@ describe 'Devise pages' do
     context 'when authorization is invalid' do
       before { click_button 'SignIn' }
 
-      it { should have_selector('div.alert.alert-warning') }
+      it { should have_selector('div.alert.alert-alert') }
       it { should have_content('Invalid') }
 
     end
@@ -60,11 +65,71 @@ describe 'Devise pages' do
       end
 
       it { should have_selector('div.alert.alert-notice') }
-      it { should have_content('Success') }
+      it { should have_content('Signed in') }
 
     end
   end
 
   describe 'Edit page' do
+    before do
+      sign_in user
+      visit edit_user_registration_path
+    end
+
+    it { should have_content('Edit User') }
+    it { should have_field('Email') }
+    it { should have_field('Password') }
+
+    context 'when invalid editing' do
+      before { click_button 'Update' }
+
+      it { should have_content('problems') }
+    end
+
+    context 'when valid editing' do
+      let(:new_username) { 'newUsername' }
+
+      before do
+        fill_in 'Email', with: user.email
+        fill_in 'Username', with: new_username
+        fill_in 'Current password', with: user.password
+        click_button 'Update'
+      end
+
+      specify { expect { user.reload.username }.eql? new_username }
+      it { should have_content('updated') }
+      it { should have_selector('div.alert.alert-notice') }
+
+    end
   end
+
+  describe 'Forgot password page' do
+
+    # Need to finish
+
+    let(:send_button) { 'Send instructions' }
+
+    before { visit new_user_password_path }
+
+    context 'when form invalid' do
+      before { click_button send_button }
+
+      it { should have_content('problems') }
+      it { should have_selector('div.alert.alert-danger') }
+
+    end
+
+    context 'when form is valid' do
+      before do
+        fill_in 'Email', with: user.email
+        click_button send_button
+        @email = ActionMailer::Base.deliveries.last
+      end
+
+      it { should have_selector('div.alert.alert-notice') }
+      it { should have_content('You will receive an email') }
+
+    end
+  end
+
 end
