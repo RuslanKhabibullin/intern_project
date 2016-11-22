7b83fe3287751e98a4faf87ed7c01364cd3c4802
RuslanKhabibullin <khabibullin.ruslan.95@gmail.com>
2016-11-22 14:36:33 +0300

    google oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable,
+         :validatable, :omniauthable, omniauth_providers: %i(google_oauth2)
 
   has_many :articles, foreign_key: "author_id", dependent: :destroy
   has_many :comments, dependent: :destroy
@@ -7,4 +8,14 @@ class User < ApplicationRecord
   has_many :liked_articles, through: :likes, source: :article
 
   validates :username, presence: true, length: { minimum: 2, maximum: 90 }
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+
+    where(email: data["email"]).first_or_create do |user|
+      user.username = data["name"]
+      user.email = data["email"]
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
---
+++ db/migrate/20161122112640_remove_unique_index_from_user.rb
@@ -0,0 +1,5 @@
+class RemoveUniqueIndexFromUser < ActiveRecord::Migration[5.0]
+  def change
+    remove_index :users, :username
+  end
+end
