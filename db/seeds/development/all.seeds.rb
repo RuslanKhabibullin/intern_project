884b039ebba12e4849e556f08c3ca361bc59ad3f
RuslanKhabibullin <khabibullin.ruslan.95@gmail.com>
2016-12-07 11:51:04 +0300

    seeds style fix

--- db/seeds/development/all.seeds.rb
+++ db/seeds/development/all.seeds.rb
@@ -1,5 +1,5 @@
-commenter = User.create(email: 'user_1@example.com', username: 'user_1',
-                        password: '123456', password_confirmation: '123456')
+commenter = User.create(email: "user_1@example.com", username: "user_1",
+                        password: "123456", password_confirmation: "123456")
 
 3.times { FactoryGirl.create(:user) }
 
